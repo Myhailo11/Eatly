8693b3bea7e68f85bb4e32a7b6768a1a53011e46
Myhailo11 <myhailo.bielov@gmail.com>
2024-03-12 20:07:47 +0200

    12032024

---
+++ test/index.js
@@ -0,0 +1,16 @@
+const ingredients = [
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
+  ];
+  
+  ingredients.forEach((item)=>[
+const ingredients = document.createElements("li");
+ingredient.textContent = item;
+ingredients.classList.add("item-");
+  ]
+  
+  )
